b0c95b01ea4bc3ade464a89fb85f46e18c45c8a6
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-17 17:52:52 -0400

    Implement axios to get days data and set days state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
+import axios from "axios";
+
 
 const appointments = {
   "1": {
@@ -42,28 +45,19 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-
 export default function Application(props) {
   
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  
+  useEffect (() => {
+    const URL = `http://localhost:8001/api/days`;
+    axios
+      .get(URL)
+      .then(response => {
+        setDays(response.data);
+      })
+  }, [])
 
   return (
     <main className="layout">
