2e72cdd8872e6bf652cb21d17aa0c1e815493636
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-16 22:51:38 -0400

    Fix form create reset bug

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -4,8 +4,10 @@ import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
   
+  console.log(props);
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  console.log(props.student, student);
 
   const reset = () => {
     setStudent("");
@@ -25,20 +27,24 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder={student || "Enter Student Name"}
-          //  student={props.student}
+            placeholder={"Enter Student Name"}
+            //don't need this line because student already set in ln8
+            // student={props.student}
+            //state of form is student as a starting value
+            value={student}
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
-          value={interviewer}
+          selectedInterviewerId={interviewer}
           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
+          {/* ()=> callback executes on "onClick" and not right away */}
           <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
