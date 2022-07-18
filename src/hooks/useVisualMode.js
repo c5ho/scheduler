4fb6244433d5a7b399f5110acab5afee26ffde8b
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-18 15:38:55 -0400

    Contruct custom hook using TDD and Jest

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    console.log("before:", history, mode, replace);
+    if (replace) {
+      const temp = [...history];
+      temp.pop();
+      setHistory(temp)
+      console.log("if replace is true:", history)
+    }
+    setMode(mode)
+    //setHistory([...history, mode])
+    setHistory((prev) => {
+      console.log("after:", [...prev, mode])
+      return [...prev, mode]})
+    //console.log("after:", history, mode, replace);
+
+  }
+
+  function back () {
+    if (history.length > 1) {
+      console.log(history, mode);
+      setMode(history[history.length - 2]);
+      const temp = [...history];
+      temp.pop();
+      setHistory(temp)
+    }
+  }
+
+  return { mode, transition, back };
+}
