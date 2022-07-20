3620f64f9baa457595bf4ef9a47639c41fe38776
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-20 17:55:06 -0400

    Unit test Form component and add error-handling

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -6,10 +6,12 @@ export default function Form(props) {
   
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
     setInterviewer(null);
+    setError("");
   }
 
   const cancel = () => {
@@ -17,22 +19,35 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer")
+      return;
+    }
+    props.onSave(student, interviewer);
+    setError("");
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-          <input
+          <input data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder={"Enter Student Name"}
-            //don't need this line because student already set in ln8
-            // student={props.student}
-            //state of form is student as a starting value
+        
+            //setStudent set student as a starting value or blank if student name does not exist
             value={student}
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           selectedInterviewerId={interviewer}
@@ -42,8 +57,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          {/* ()=> callback executes on "onClick" and not right away */}
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
